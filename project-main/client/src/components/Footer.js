8357ee37bd50a3e7bd1e58d341dfab363f2b63e3
Aryan Chaudhary <aryan1411chaudhary@gmail.com>
2025-09-20 21:05:50 +0530

    edit

--- project-main/client/src/components/Footer.js
+++ project-main/client/src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
         <Typography variant="subtitle1">
           Don't forget to star the{" "}
           <a
-            href="https://github.com/ihtasham42/social-media-app"
+            href="https://github.com/chaudharyaryan10/social_media_app"
             target="_blank"
           >
             Repo
